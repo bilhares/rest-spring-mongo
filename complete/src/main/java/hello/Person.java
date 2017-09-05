65d3935423126bb26acf223694e4f30e51b82b35
bilhares <felipe.bilhares@hotmail.com>
2017-09-05 17:05:30 -0400

    commit final

---
+++ complete/src/main/java/hello/Person.java
@@ -0,0 +1,56 @@
+package hello;
+
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+
+public class Person {
+
+	@Id private String id;
+
+	private String firstName;
+	private String lastName;
+	private List<Habilidades> habilidades;
+	private List<Double> notas;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public List<Habilidades> getHabilidades() {
+		return habilidades;
+	}
+
+	public void setHabilidades(List<Habilidades> habilidades) {
+		this.habilidades = habilidades;
+	}
+
+	public List<Double> getNotas() {
+		return notas;
+	}
+
+	public void setNotas(List<Double> notas) {
+		this.notas = notas;
+	}
+	
+}
